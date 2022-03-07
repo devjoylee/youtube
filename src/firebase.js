93869565fa69c7292bde671e6a499d30e7226e19
Joy Lee <joylee.dev@gmail.com>
2022-03-08 00:24:46 +0900

    ➕ install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from './firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-QsAnaeZ_EFaj3omY2KjelFsYdnW9Fjw',
+  authDomain: 'clone-4f990.firebaseapp.com',
+  projectId: 'clone-4f990',
+  storageBucket: 'clone-4f990.appspot.com',
+  messagingSenderId: '412615046248',
+  appId: '1:412615046248:web:cd5f3b2adc8e519cd3f7dd',
+};
+
+firebase.initializeApp(firebaseConfig);
