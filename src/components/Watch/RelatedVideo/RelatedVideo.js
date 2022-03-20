d77aae84f64b89368ef4ac81cde099b72ca7105e
Joy Lee <joylee.dev@gmail.com>
2022-03-20 16:08:17 +0900

    ✨ display related videos on Watch Page

--- src/components/Watch/RelatedVideo/RelatedVideo.js
+++ src/components/Watch/RelatedVideo/RelatedVideo.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import numeral from 'numeral';
+import { getData } from 'utils/getData';
+import { useNavigate } from 'react-router-dom';
 
-export const RelatedVideo = () => {
+export const RelatedVideo = ({ video }) => {
+  const {
+    id,
+    snippet: { channelTitle, title, publishedAt, thumbnails },
+  } = video;
+
+  const [views, setViews] = useState(null);
+  const [duration, setDuration] = useState(null);
+
+  const seconds = moment.duration(duration).asSeconds();
+  const _duration = moment.utc(seconds * 1000).format('mm:ss');
+  const _views = numeral(views).format('0.a');
+  const publishedTime = moment(publishedAt).fromNow();
+  const thumbnail = thumbnails.default.url;
+
+  const videoId = id?.videoId || id;
+
+  const navigate = useNavigate();
+
+  const handleVideoClick = () => {
+    navigate(`/watch/${videoId}`);
+  };
+
+  useEffect(() => {
+    const get_video_details = async () => {
+      const {
+        data: { items },
+      } = await getData('/videos', {
+        params: {
+          part: 'contentDetails,statistics',
+          id: videoId,
+        },
+      });
+      setDuration(items[0].contentDetails.duration);
+      setViews(items[0].statistics.viewCount);
+    };
+    get_video_details();
+  }, [videoId]);
+
   return (
-    <li className='related_video'>
+    <li className='related_video' onClick={handleVideoClick}>
       <div className='related_video__thumbnail'>
-        <img src='https://dailyblogger.com/wp-content/uploads/2019/02/Best_YouTube_Thumbnail_Tool.png' alt='' />
-        <span className='duration'>03:10</span>
+        <img src={thumbnail} alt='thumbnail' />
+        <span className='duration'>{_duration}</span>
       </div>
       <div className='related_video__info'>
-        <p className='title'>Be ad full stack developer in a month</p>
-        <p className='channel'>Joy Lee</p>
+        <p className='title'>{title}</p>
+        <p className='channel'>{channelTitle}</p>
         <p className='details'>
-          {numeral(1000000).format('0.a')} Views • {moment('2022-01-01').fromNow()}
+          {_views} Views • {publishedTime}
         </p>
       </div>
     </li>
--- src/components/Watch/RelatedVideo/RelatedVideoList.js
+++ src/components/Watch/RelatedVideo/RelatedVideoList.js
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getRelatedVideo } from 'redux/actions';
 import { RelatedVideo } from './RelatedVideo';
 import './_relatedVideo.scss';
 
 export const RelatedVideoList = ({ id }) => {
   const dispatch = useDispatch();
+  const { videos } = useSelector((state) => state.relatedVideo);
 
   useEffect(() => {
     dispatch(getRelatedVideo(id));
   }, [dispatch, id]);
 
+  console.log(videos);
   return (
     <ul className='related_video_list'>
-      {[...Array(10)].map((_, i) => (
-        <RelatedVideo key={i} />
-      ))}
+      {videos.map((video, i) => video.snippet && <RelatedVideo key={i} video={video} />)}
     </ul>
   );
 };
